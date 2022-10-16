97da84490ff1bcbbdcb581ae2211949f91a16567
HemangajitDutta <hemangodutta7@gmail.com>
2022-10-16 21:52:33 +0530

    learning javascript

---
+++ script.js
@@ -0,0 +1,15 @@
+// learning about variables
+
+// There are 3 types of variables in javascript. They are as follows 
+
+var // Before 2015 var was the only way to define a variable. The problem with var was that variable declaring var could be reassigned with another variable thus making it harder for debugging.
+let 
+///const
+var a = "Rahul"
+var a = "Hemango"
+console.log(a)
+const b = "CHERRY"
+console.log(b)
+
+
+document.getElementById("demo2").innerHTML=a
