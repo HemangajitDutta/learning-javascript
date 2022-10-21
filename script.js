ad3ff1f4fab3649fcba510010b9298a5c046361c
HemangajitDutta <hemangodutta7@gmail.com>
2022-10-21 22:09:41 +0530

    learning function

--- script.js
+++ script.js
@@ -13,3 +13,20 @@ console.log(b)
 
 
 document.getElementById("demo2").innerHTML=a
+let sum= 1+2
+console.log(sum)
+let first = 23
+let second = "23"
+if (first === second) {
+    console.log("true")
+}else {console.log("false")}
+// A javascript function is a block of code design to perform a particular task
+let multiply= hemango(4,5)
+function hemango( x,y) {
+    return x * y
+} 
+console.log(multiply)
+document.getElementById("demo3").innerHTML = toCelsius(40)
+ function toCelsius(f) {
+    return (5/9) * (f-32)
+ }
